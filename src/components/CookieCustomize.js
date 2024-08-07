6fbf81b344017ee8864649031b9fed6dd81a54ea
Peter <101063969+Slowpokepeter@users.noreply.github.com>
2024-08-07 11:53:17 +0100

    added slide style

--- src/components/CookieCustomize.js
+++ src/components/CookieCustomize.js
@@ -1,3 +1,4 @@
+// src/components/CookieCustomize.js
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './CookieConsentBanner.css';
@@ -14,73 +15,76 @@ const CookieCustomize = ({ preferences, onSave, acceptButtonColor, acceptButtonT
     onSave(localPreferences);
   };
 
+  const sliderStyle = {
+    backgroundColor: acceptButtonColor,
+  };
+
   return (
     <div className="cookie-customize">
       <h3>Cookie Settings</h3>
       <p className="p-cookies">We use cookies to enhance your experience, analyze traffic, and deliver personalized content.</p>
       <div className='custom-wrap'>
         <div className='custom-setting'>
-            Analytics
-            <label className='switch'>
-                <input
-                type="checkbox"
-                name="analytics"
-                checked={localPreferences.analytics}
-                onChange={handleChange}
-                />
-                <span className='slider round'></span>
-            </label>
+          Analytics
+          <label className='switch'>
+            <input
+              type="checkbox"
+              name="analytics"
+              checked={localPreferences.analytics}
+              onChange={handleChange}
+            />
+            <span className='slider round' style={localPreferences.analytics ? sliderStyle : null}></span>
+          </label>
         </div>
         <div className='custom-setting'>
-            Marketing
-            <label className='switch'>
-                <input
-                type="checkbox"
-                name="marketing"
-                checked={localPreferences.marketing}
-                onChange={handleChange}
-                />
-                <span className='slider round'></span>
-            </label>
+          Marketing
+          <label className='switch'>
+            <input
+              type="checkbox"
+              name="marketing"
+              checked={localPreferences.marketing}
+              onChange={handleChange}
+            />
+            <span className='slider round' style={localPreferences.marketing ? sliderStyle : null}></span>
+          </label>
         </div>
         <div className='custom-setting'>
-            Functionality
-            <label className='switch'>
-                <input
-                type="checkbox"
-                name="functionality"
-                checked={localPreferences.functionality}
-                onChange={handleChange}
-                />
-                <span className='slider round'></span>
-            </label>
+          Functionality
+          <label className='switch'>
+            <input
+              type="checkbox"
+              name="functionality"
+              checked={localPreferences.functionality}
+              onChange={handleChange}
+            />
+            <span className='slider round' style={localPreferences.functionality ? sliderStyle : null}></span>
+          </label>
         </div>
         <div className='custom-setting'>
-            Personalization
-            <label className='switch'>
-                <input
-                type="checkbox"
-                name="personalization"
-                checked={localPreferences.personalization}
-                onChange={handleChange}
-                />
-                <span className='slider round'></span>
-            </label>
+          Personalization
+          <label className='switch'>
+            <input
+              type="checkbox"
+              name="personalization"
+              checked={localPreferences.personalization}
+              onChange={handleChange}
+            />
+            <span className='slider round' style={localPreferences.personalization ? sliderStyle : null}></span>
+          </label>
         </div>
         <div className='custom-setting'>
-            Security
-            <label className='switch'>
-                <input
-                type="checkbox"
-                name="security"
-                checked={localPreferences.security}
-                onChange={handleChange}
-                />
-                <span className='slider round'></span>
-            </label>
+          Security
+          <label className='switch'>
+            <input
+              type="checkbox"
+              name="security"
+              checked={localPreferences.security}
+              onChange={handleChange}
+            />
+            <span className='slider round' style={localPreferences.security ? sliderStyle : null}></span>
+          </label>
         </div>
       </div>
-      
       <button onClick={handleSave} className="save-button" style={{ backgroundColor: acceptButtonColor, color: acceptButtonTextColor }}>Save Preferences</button>
     </div>
   );
