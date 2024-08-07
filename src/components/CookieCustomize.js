036f1af10f49e6c0b0789d1311658d2dfc49a9cb
Peter <101063969+Slowpokepeter@users.noreply.github.com>
2024-08-07 11:45:45 +0100

    adjusted for all settings

--- src/components/CookieCustomize.js
+++ src/components/CookieCustomize.js
@@ -14,43 +14,74 @@ const CookieCustomize = ({ preferences, onSave, acceptButtonColor, acceptButtonT
     onSave(localPreferences);
   };
 
-  const sliderStyle = {
-    backgroundColor: acceptButtonColor,
-  };
-
   return (
     <div className="cookie-customize">
       <h3>Cookie Settings</h3>
-      <p className="p-cookies">We use cookies to enhance your experience, analyze traffic and deliver personalized content.</p>
+      <p className="p-cookies">We use cookies to enhance your experience, analyze traffic, and deliver personalized content.</p>
       <div className='custom-wrap'>
         <div className='custom-setting'>
-          Analytics
-          <label className='switch'>
-            <input
-              type="checkbox"
-              name="analytics"
-              checked={localPreferences.analytics}
-              onChange={handleChange}
-            />
-            <span className='slider round' style={localPreferences.analytics ? sliderStyle : null}></span>
-          </label>
+            Analytics
+            <label className='switch'>
+                <input
+                type="checkbox"
+                name="analytics"
+                checked={localPreferences.analytics}
+                onChange={handleChange}
+                />
+                <span className='slider round'></span>
+            </label>
         </div>
         <div className='custom-setting'>
-          Marketing
-          <label className='switch'>
-            <input
-              type="checkbox"
-              name="marketing"
-              checked={localPreferences.marketing}
-              onChange={handleChange}
-            />
-            <span className='slider round' style={localPreferences.marketing ? sliderStyle : null}></span>
-          </label>
+            Marketing
+            <label className='switch'>
+                <input
+                type="checkbox"
+                name="marketing"
+                checked={localPreferences.marketing}
+                onChange={handleChange}
+                />
+                <span className='slider round'></span>
+            </label>
+        </div>
+        <div className='custom-setting'>
+            Functionality
+            <label className='switch'>
+                <input
+                type="checkbox"
+                name="functionality"
+                checked={localPreferences.functionality}
+                onChange={handleChange}
+                />
+                <span className='slider round'></span>
+            </label>
+        </div>
+        <div className='custom-setting'>
+            Personalization
+            <label className='switch'>
+                <input
+                type="checkbox"
+                name="personalization"
+                checked={localPreferences.personalization}
+                onChange={handleChange}
+                />
+                <span className='slider round'></span>
+            </label>
+        </div>
+        <div className='custom-setting'>
+            Security
+            <label className='switch'>
+                <input
+                type="checkbox"
+                name="security"
+                checked={localPreferences.security}
+                onChange={handleChange}
+                />
+                <span className='slider round'></span>
+            </label>
         </div>
       </div>
-      <button onClick={handleSave} className="save-button" style={{ backgroundColor: acceptButtonColor, color: acceptButtonTextColor }}>
-        Save Preferences
-      </button>
+      
+      <button onClick={handleSave} className="save-button" style={{ backgroundColor: acceptButtonColor, color: acceptButtonTextColor }}>Save Preferences</button>
     </div>
   );
 };
@@ -59,7 +90,7 @@ CookieCustomize.propTypes = {
   preferences: PropTypes.object.isRequired,
   onSave: PropTypes.func.isRequired,
   acceptButtonColor: PropTypes.string.isRequired,
-  acceptButtonTextColor: PropTypes.string.isRequired, // Added this prop type
+  acceptButtonTextColor: PropTypes.string.isRequired,
 };
 
 export default CookieCustomize;
